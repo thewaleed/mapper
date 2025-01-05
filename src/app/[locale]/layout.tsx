a59ab1f8bf00461145a33166c897c1b48dbccc5a
thewaleed <thewaleed@live.com>
2025-01-05 23:11:12 +0100

    removed unused5

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -6,21 +6,21 @@ export function generateStaticParams() {
   return [{ locale: 'fr' }, { locale: 'ar' }, { locale: 'en' }];
 }
 
-export default async function LocaleLayout({ 
+export default async function LocaleLayout({
   children,
-  params 
+  params: { locale }
 }: {
   children: React.ReactNode;
   params: { locale: string };
 }) {
-  const messages = await import(`../../messages/${params.locale}.json`)
+  const messages = await import(`../../messages/${locale}.json`)
     .then(module => module.default)
     .catch(() => notFound());
 
   return (
-    <html lang={params.locale} suppressHydrationWarning>
+    <html lang={locale} suppressHydrationWarning>
       <body suppressHydrationWarning>
-        <NextIntlClientProvider messages={messages} locale={params.locale}>
+        <NextIntlClientProvider messages={messages} locale={locale}>
           {children}
         </NextIntlClientProvider>
       </body>
